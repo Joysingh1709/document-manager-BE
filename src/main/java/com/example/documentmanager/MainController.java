5dde24d097530edcdce90ca9f9139365a89b681a
Joy Singh <avnishmamta060606@gmail.com>
2022-01-19 20:55:58 +0530

    dp

--- src/main/java/com/example/documentmanager/MainController.java
+++ src/main/java/com/example/documentmanager/MainController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import net.minidev.json.JSONObject;
 
 @RestController
-@CrossOrigin(origins = { "http://someserver:8100" })
+@CrossOrigin(origins = { "http://localhost:8100", "http://192.168.29.38:8100" })
 public class MainController {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String index() {
-        return "Greetings from Document manager backend..!";
+        return "Greetings from Document manager backend on heroku..!";
     }
 
     @RequestMapping(path = "/api", method = RequestMethod.GET)
